50ca09d1bb55d8be2062ed246b71b38aa80f55d8
Faheem Akhtar <faheemakhtar19730@gmail.com>
2023-05-25 00:01:26 +0530

    update

--- backend/app.js
+++ backend/app.js
@@ -4,7 +4,7 @@ const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const fileUpload = require("express-fileupload");
 const path = require("path");
-const cors = rquire("cors");
+const cors = require("cors");
 const errorMiddleware = require("./middleware/error");
 
 // Config
