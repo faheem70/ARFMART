937a752329c5dee7c3d9e49f90c051dd7384e846
Faheem Akhtar <faheemakhtar19730@gmail.com>
2023-05-28 16:33:04 +0530

    update

--- backend/config/database.js
+++ backend/config/database.js
@@ -3,7 +3,11 @@ require('dotenv').config();
 mongoose.set("strictQuery", false);
 const connectDatabase = async () => {
     await mongoose
-      .connect(process.env.DB_URI)
+        .connect(process.env.DB_URI, {
+            useUnifiedTopology: true,
+            useNewUrlParser: true,
+
+        })
       .then((data) => {
           console.log(`Mongodb connected with server: ${data.connection.host}`);
       });
