1fbfd4588c58b864a6d680e854c61debdb023434
Faheem Akhtar <faheemakhtar19730@gmail.com>
2023-05-28 16:24:47 +0530

    update

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose");
 
-const connectDatabase = () => {
-    mongoose
+mongoose.set("strictQuery", false);
+const connectDatabase = async () => {
+    await mongoose
       .connect(process.env.DB_URI)
       .then((data) => {
           console.log(`Mongodb connected with server: ${data.connection.host}`);
