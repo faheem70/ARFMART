6ca980419a4b76969f2b05c9ef4509f443e91fae
Faheem Akhtar <faheemakhtar19730@gmail.com>
2023-05-28 16:10:12 +0530

    update

--- backend/app.js
+++ backend/app.js
@@ -4,7 +4,7 @@ const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const fileUpload = require("express-fileupload");
 const path = require("path");
-const cors = require("cors");
+
 const errorMiddleware = require("./middleware/error");
 
 // Config
@@ -12,13 +12,10 @@ if (process.env.NODE_ENV !== "PRODUCTION") {
   require("dotenv").config({ path: "backend/config/config.env" });
 }
 
-
+app.use(express.json());
 app.use(cookieParser());
-app.use(express.json({limit: '25mb'}));
-app.use(express.urlencoded({limit: '25mb', extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(fileUpload());
-
-
 
 // Route Imports
 const product = require("./routes/productRoute");
@@ -31,11 +28,10 @@ app.use("/api/v1", user);
 app.use("/api/v1", order);
 app.use("/api/v1", payment);
 
-app.use(cors());
-app.use(express.static(path.join(__dirname, 'frontend','build')));
+app.use(express.static(path.join(__dirname, "../frontend/build")));
+
 app.get("*", (req, res) => {
- res.sendFile(path.resolve(__dirname, 'frontend','build','index.html'));
-  //res.setHeader("Access-Control-Allow-Credentials,", "true");
+  res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
 });
 
 // Middleware for Errors
--- backend/server.js
+++ backend/server.js
@@ -1,41 +1,38 @@
-const app = require('./app');
-const mongoose = require('mongoose');
+const app = require("./app");
 const cloudinary = require("cloudinary");
+const connectDatabase = require("./config/database");
 
-mongoose.set('strictQuery', true);
-const connectDatabase = require("./config/database")
-
-process.on("uncaughtException",(err)=>{
+// Handling Uncaught Exception
+process.on("uncaughtException", (err) => {
     console.log(`Error: ${err.message}`);
-    console.log(`Shutting down the server due to Unhandled Promise Rejection`);
+    console.log(`Shutting down the server due to Uncaught Exception`);
     process.exit(1);
-})
+});
 
-if(process.env.NODE_ENV!=="PRODUCTION"){
-    require('dotenv').config({path:"backend/config/config.env"});
+// Config
+if (process.env.NODE_ENV !== "PRODUCTION") {
+    require("dotenv").config({ path: "backend/config/config.env" });
 }
 
-//conect database
+// Connecting to database
 connectDatabase();
+
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
     api_secret: process.env.CLOUDINARY_API_SECRET,
-  });
-
-const server = app.listen(process.env.PORT,()=>{
-    console.log(`server is running http://localhost:${process.env.PORT}`);
 });
 
+const server = app.listen(process.env.PORT, () => {
+    console.log(`Server is working on http://localhost:${process.env.PORT}`);
+});
 
-// unHandled Promise Rejection
-
-process.on("unhandledRejection", err=>{
+// Unhandled Promise Rejection
+process.on("unhandledRejection", (err) => {
     console.log(`Error: ${err.message}`);
     console.log(`Shutting down the server due to Unhandled Promise Rejection`);
 
-    server.close(()=>{
+    server.close(() => {
         process.exit(1);
     });
-
 });
