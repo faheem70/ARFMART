1715419e9b5194a42aadedf952a1adb4862ab7d7
Faheem Akhtar <faheemakhtar19730@gmail.com>
2023-05-28 16:16:28 +0530

    update

--- backend/config/database.js
+++ backend/config/database.js
@@ -2,12 +2,10 @@ const mongoose = require("mongoose");
 
 const connectDatabase = () => {
     mongoose
-        .connect(process.env.DB_URI, {
-
-        })
-        .then((data) => {
-            console.log(`Mongodb connected with server: ${data.connection.host}`);
-        });
+      .connect(process.env.DB_URI)
+      .then((data) => {
+          console.log(`Mongodb connected with server: ${data.connection.host}`);
+      });
 };
 
 module.exports = connectDatabase;
