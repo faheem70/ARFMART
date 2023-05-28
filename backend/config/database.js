8f7460095a185f70d8e33b4ba4c2ba77b3a05691
Faheem Akhtar <faheemakhtar19730@gmail.com>
2023-05-28 15:43:01 +0530

    update

--- backend/config/database.js
+++ backend/config/database.js
@@ -4,11 +4,11 @@ const dotenv = require("dotenv");
 
 dotenv.config();
 const path = require('path');
-require('dotenv').config({ path: path.resolve(__dirname, './.env') });
+require('dotenv').config({ path: path.resolve(__dirname, './.env') })
 
 const connectDatabase = () => {
     mongoose.connect(process.env.DB_URI).then((data) => {
-        console.log(`Mongodb connected with server : ${data.connection.host}`);
+        console.log(`Mongodb connected with server : ${data.connection.host}`)
         });
 };
 module.exports = connectDatabase
