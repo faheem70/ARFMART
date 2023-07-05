21facf9a074285cfe53262f0b427f5d07c63f6de
Faheem Akhtar <faheemakhtar19730@gmail.com>
2023-07-06 01:50:45 +0530

    update

--- backend/webpack.config.js
+++ backend/webpack.config.js
@@ -1,40 +1,25 @@
-const HtmlWebPackPlugin = require('html-webpack-plugin');
-const path = require('path');
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    context: __dirname,
-    entry: './src/index.js',
+    entry: './app/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'main.js',
-        publicPath: '/',
-    },
-    devServer: {
-        historyApiFallback: true,
-        contentBase: './',
-        hot: true
+        filename: 'index_bundle.js',
+        publicPath: '/'
     },
     module: {
         rules: [
-            {
-                test: /\.js?$/,
-                exclude: /node_module/,
-                use: 'babel-loader'
-            },
-            {
-                test: /\.css?$/,
-                use: ['style-loader', 'css-loader']
-            },
-            {
-                test: /\.(png|j?g|svg|gif)?$/,
-                use: 'file-loader'
-            }
+            { test: /\.(js)$/, use: 'babel-loader' },
+            { test: /\.css$/, use: ['style-loader', 'css-loader'] }
         ]
     },
+    devServer: {
+        historyApiFallback: true,
+    },
     plugins: [
-        new HtmlWebPackPlugin({
-            template: path.resolve(__dirname, 'public/index.html'),
-            filename: 'index.html'
-        })
-    ]
+      new HtmlWebpackPlugin({
+          template: 'app/index.html'
+      })
+  ]
 };
