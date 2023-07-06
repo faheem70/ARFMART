df948dd6d462aab73f4e82d3567b5e69a27b2a98
Faheem Akhtar <faheemakhtar19730@gmail.com>
2023-07-07 00:54:35 +0530

    update

--- backend/app.js
+++ backend/app.js
@@ -72,10 +72,10 @@ app.use("/api/v1", user);
 app.use("/api/v1", order);
 app.use("/api/v1", payment);
 
-app.use(express.static(path.join(__dirname, "../frontend/build")));
+app.use(express.static(path.join(__dirname, "/public/build")));
 
 app.get("/*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
+  res.sendFile(path.resolve(__dirname, "/public/index.html"));
 });
 
 
