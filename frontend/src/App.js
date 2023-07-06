c39104403366a313260c648be424c0824eb45283
Faheem Akhtar <faheemakhtar19730@gmail.com>
2023-07-06 11:42:48 +0530

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 
 import './App.css';
 import Header from "./component/layout/Header/Header.js"
-import { browserHistory, BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import WebFont from "webfontloader";
 import React, { useState, useEffect } from 'react';
 import Footer from './component/layout/Footer/Footer.js';
@@ -74,7 +74,7 @@ function App() {
   }, []);
   return (
 
-    <Router history={browserHistory}>
+    <Router >
       <Header />
       {isAuthenticated && <UserOptions user={user} />}
       <Routes>
