72f5ea0d7b591c8255e9a9ef08921ff0e25222f2
Faheem Akhtar <faheemakhtar19730@gmail.com>
2023-07-06 11:24:32 +0530

    update

--- backend/app.js
+++ backend/app.js
@@ -53,8 +53,7 @@ const errorMiddleware = require("./middleware/error");
 if (process.env.NODE_ENV !== "PRODUCTION") {
   require("dotenv").config({ path: "backend/config/config.env" });
 }
-app.use(historyApiFallback({ verbose: true }))
-
+app.use(history());
 app.use(express.json());
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
