17ed724aa13b7626f465f64f3b063564bf1d22ec
Faheem Akhtar <faheemakhtar19730@gmail.com>
2023-07-06 11:31:29 +0530

    update

--- backend/app.js
+++ backend/app.js
@@ -72,9 +72,10 @@ app.use("/api/v1", payment);
 
 app.use(express.static(path.join(__dirname, "../frontend/build")));
 
-app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
 });
+
 
 // Middleware for Errors
 app.use(errorMiddleware);
