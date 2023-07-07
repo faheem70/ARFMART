ae064278ce83e42a3bb2ae4cc4ded11baac937e4
Faheem Akhtar <faheemakhtar19730@gmail.com>
2023-07-08 00:46:14 +0530

    update

--- backend/app.js
+++ backend/app.js
@@ -55,6 +55,7 @@ if (process.env.NODE_ENV !== "PRODUCTION") {
   require("dotenv").config({ path: "backend/config/config.env" });
 }
 app.use(cors({
+  credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE"]
 }));
 app.use(history());
