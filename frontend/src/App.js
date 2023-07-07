aeff8b7bf7170041340d39f975704fade82a61b1
Faheem Akhtar <faheemakhtar19730@gmail.com>
2023-07-07 15:47:36 +0530

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -52,7 +52,7 @@ function App() {
 
   async function getStripeApiKey() {
     try {
-      const { data } = await axios.get(`https://arfmart.onrender.com/api/v1/stripeapikey`);
+      const { data } = await axios.get(`/api/v1/stripeapikey`);
 
       setStripeApiKey(data.stripeApiKey);
     } catch (error) {
