7ee85b75bf2f516263d20183155cc664b91624c6
Faheem Akhtar <faheemakhtar19730@gmail.com>
2023-07-07 14:36:23 +0530

    update

--- frontend/src/component/Home/Home.js
+++ frontend/src/component/Home/Home.js
@@ -1,59 +1,57 @@
-import React, {Fragment, useEffect} from "react";
-//import {CgMouse} from "react-icons/all";
+import React, { Fragment, useEffect } from "react";
+import { CgMouse } from "react-icons/all";
 import "./Home.css";
-import Product from "./ProductCard.js";
-import MetaData  from "../layout/Metadata";
-import {clearErrors, getProduct} from "../../actions/productAction";
-import {useDispatch, useSelector} from "react-redux";
+import ProductCard from "./ProductCard.js";
+import MetaData from "../layout/MetaData";
+import { clearErrors, getProduct } from "../../actions/productAction";
+import { useSelector, useDispatch } from "react-redux";
 import Loader from "../layout/Loader/Loader";
 import { useAlert } from "react-alert";
 
-const Home = ()=>{
+const Home = () => {
   const alert = useAlert();
   const dispatch = useDispatch();
- const {loading, error, products} = useSelector(
-  (state)=>state.products
- );
-  useEffect(()=>{
+  const { loading, error, products } = useSelector((state) => state.products);
+
+  useEffect(() => {
     if (error) {
       alert.error(error);
       dispatch(clearErrors());
     }
     dispatch(getProduct());
-  },[dispatch,error,alert]);
-      return (
-        
-        <Fragment>
-          {loading ?(
-            <Loader/>
-          ) :(
-   <Fragment>
-     
-     
-     <MetaData title="ARF MART"/>
+  }, [dispatch, error, alert]);
 
-    <div className="banner">
-      <p>Welcome to ARF MART</p>
-      <h1>FIND AMAZING PRODUCTS BELOW</h1>
-      
+  return (
+    <Fragment>
+      {loading ? (
+        <Loader />
+      ) : (
+        <Fragment>
+            <MetaData title="ECOMMERCE" />
+
+            <div className="banner">
+              <p>Welcome to Ecommerce</p>
+              <h1>FIND AMAZING PRODUCTS BELOW</h1>
+
             <a href="#container">
               <button>
-                Scroll 
+                  Scroll <CgMouse />
               </button>
             </a>
-    </div>
-    <h2 className="homeHeading">Featured Products</h2>
-    <div className="container" id="container">
+            </div>
+
+            <h2 className="homeHeading">Featured Products</h2>
+
+            <div className="container" id="container">
             {products &&
               products.map((product) => (
-                <Product product={product} />
+                <ProductCard key={product._id} product={product} />
               ))}
           </div>
-   </Fragment>
+          </Fragment>
       )}
-      </Fragment>
+    </Fragment>
   );
 };
-
 
 export default Home;
