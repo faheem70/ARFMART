1e732e71cb15825e477a60a3879b76566b8f15bb
Faheem Akhtar <faheemakhtar19730@gmail.com>
2023-07-08 11:25:19 +0530

    update

--- backend/app.js
+++ backend/app.js
@@ -56,7 +56,7 @@ if (process.env.NODE_ENV !== "PRODUCTION") {
 }
 app.options('*', cors());
 const corsOptions = {
-  ["http://localhost:4000", "http://localhost:3000"],
+  origin: ["http://localhost:4000", "http://localhost:3000"],
   credentials: true,            //access-control-allow-credentials:true
   optionSuccessStatus: 200,
 }
